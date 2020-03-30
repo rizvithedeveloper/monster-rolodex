ffdff31230503bff9eb865a0f8e897b58819efb9
rizvithedeveloper <rizwan.xammin@gmail.com>
2020-03-30 19:14:40 +0500

    Updates

---
+++ precache-manifest.dcd6192657f4b27ceced520a08a24371.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "27c7e5c0072c817af60e06a6b9b5ad3a",
+    "url": "/monster-rolodex/index.html"
+  },
+  {
+    "revision": "3ce648021ca1f5767918",
+    "url": "/monster-rolodex/static/css/main.f0f42303.chunk.css"
+  },
+  {
+    "revision": "1bb5dd2a6657ff2ee685",
+    "url": "/monster-rolodex/static/js/2.b6f2604e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monster-rolodex/static/js/2.b6f2604e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3ce648021ca1f5767918",
+    "url": "/monster-rolodex/static/js/main.5a2afb4a.chunk.js"
+  },
+  {
+    "revision": "90506cc279e62bb14608",
+    "url": "/monster-rolodex/static/js/runtime-main.59621b72.js"
+  }
+]);
